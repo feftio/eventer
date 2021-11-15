c7ef1cba6db8683b90c84ace17cb29d729ab8f10
Eduard Lik <feft99@gmail.com>
2021-11-16 02:26:42 +0600

    Create vue project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="number"/>
+    <button @click="clicking" v-show="message == ''" style="width: 100px; height: 50px;">{{number}}</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    clicking () {
+      this.message = Math.random()
+    }
+  },
+  computed: {
+    number () {
+      return 'Number is: ' + this.message
+    }
+  },
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
