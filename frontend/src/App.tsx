dcc767d0f470d1aa4cff0390d8c51ade531b0f2e
Eduard <feft99@gmail.com>
2022-04-19 05:37:32 +0600

    frontend: import general.scss, create theme mui, change routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,39 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./store";
 import HomePage from "./pages/home";
-import LoginPage from "./pages/login";
+import AuthPage from "./pages/auth";
 import NotFoundPage from "./pages/notfound";
-import RegistrationPage from "./pages/registration";
-import "antd/dist/antd.min.css";
+import "styles/general.scss";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#3a54aa",
+        },
+        secondary: {
+            main: "#b0b3c4",
+        },
+        tonalOffset: 0.2,
+    },
+    components: {},
+});
 
 const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="*" element={<NotFoundPage />} />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/auth" element={<AuthPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </ThemeProvider>
+    );
 };
 
 export default App;
