3ec4a9407ab3e8936808d0286d0deaabcee4c758
Eduard <feft99@gmail.com>
2022-05-01 04:12:27 +0600

    frontend: testing home page when authenticated

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,7 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { useRootDispatch, useRootSelector } from "src/redux";
+import { logout } from "src/redux/auth/functions";
+import Swal from "src/swal";
 
 const HomePage: React.FC<{}> = () => {
-    return <div>HomePage</div>;
+    const dispatch = useRootDispatch();
+    const authenticated = useRootSelector((state) => state.auth.authenticated);
+    if (authenticated)
+        return (
+            <>
+                <div>HomePage: Authenticated</div>
+                <button
+                    onClick={() => {
+                        dispatch(logout());
+                    }}
+                >
+                    Logout
+                </button>
+            </>
+        );
+    return <button onClick={() => {}}>HomePage</button>;
 };
 
 export default HomePage;
