a7cf99a97c0f6ccd7e5b0398ea474be3ad9ce035
Lik Eduard <feft99@gmail.com>
2022-05-01 20:05:29 +0600

    frontend: bug fix for react 18 version

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,12 @@
-import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById("root")
+    </Provider>
 );
